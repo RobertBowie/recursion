6953704a7b94b79d3f0c1ae965a2e463be030907
RobertBowie <robertb1903@gmail.com>
2015-09-17 18:11:52 -0700

    Initial setup of vars and a couple if's/comments.

--- src/getElementsByClassName.js
+++ src/getElementsByClassName.js
@@ -4,7 +4,32 @@
 // };
 
 // But instead we're going to implement it from scratch:
-var getElementsByClassName = function(className
-){
-  // your code here
+var getElementsByClassName = function(className){
+//-----------------------------------------------------------
+// Setting up shop.
+//-----------------------------------------------------------
+  var result = [];
+  var node = node || document.body;
+//-----------------------------------------------------------
+// Initial check for target class.
+//-----------------------------------------------------------
+  if (node.classList && node.classList.contains(className)) {
+    result.push(node);
+  }
+//-----------------------------------------------------------
+// Check for children and move to Phase II
+//-----------------------------------------------------------
+  var children = node.children ? node.children : null;
+  if (children) {
+    //
+  }
+//-----------------------------------------------------------
+// 
+//-----------------------------------------------------------
+
+
+//-----------------------------------------------------------
+// 
+//-----------------------------------------------------------
+  return result;
 };
